1d734acee36b572f6c2a5125d6d5e3c6b553a34f
Gururaja M <gururajkumar161@gmail.com>
2025-06-23 02:04:20 +0530

    Added egg timer with functionality

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,6 +10,25 @@ import UIKit
 
 class ViewController: UIViewController {
     
-
-
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    
+    let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
+    var secondRemaining: Int = 60
+    var timer = Timer()
+    @IBAction func keyPessed(_ sender: UIButton) {
+        timer.invalidate()
+        secondRemaining = eggTimes[sender.currentTitle!] ?? 0
+        timer =  Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+    }
+    
+    @objc func updateTimer() {
+        if secondRemaining > 0 {
+            secondRemaining -= 1
+            
+        } else {
+            timer.invalidate()
+            self.titleLabel.text = "Done!"
+        } }
+    
 }
