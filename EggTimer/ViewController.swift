6776c2e6a43857352cc3c16092e955d1f3ad547b
Gururaja M <gururajkumar161@gmail.com>
2025-06-23 02:28:34 +0530

    Added the progress bar with functionality

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -10,21 +10,30 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    @IBOutlet weak var progressView: UIProgressView!
     @IBOutlet weak var titleLabel: UILabel!
     
     
     let eggTimes = ["Soft": 3, "Medium": 4, "Hard": 7]
     var secondRemaining: Int = 60
     var timer = Timer()
+    var timeStart = 0
+    
     @IBAction func keyPessed(_ sender: UIButton) {
         timer.invalidate()
-        secondRemaining = eggTimes[sender.currentTitle!] ?? 0
+        timeStart = eggTimes[sender.currentTitle!] ?? 0
+        
+        progressView.setProgress(0, animated: false)
+        titleLabel.text = "\(sender.currentTitle!) Cooking..."
+        secondRemaining = 0
+        
         timer =  Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
     }
     
     @objc func updateTimer() {
-        if secondRemaining > 0 {
-            secondRemaining -= 1
+        if secondRemaining < timeStart {
+            secondRemaining += 1
+            progressView.progress = Float(secondRemaining) / Float(timeStart)
             
         } else {
             timer.invalidate()
